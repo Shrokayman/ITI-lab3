15e2a674a541c407990d47cd0b13699c27fc2195
Shrok Ayman <shrokayman99@gmail.com>
2022-01-18 21:35:14 +0200

    My simple api project

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -5,8 +5,116 @@ const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
 
 
+// Get user by id
+router.get("/:id", async (req, res, next) => {
+    try {
+        const id = req.params.id
+        console.log(id);
+        const users = await fs.promises.readFile('./user.json', { encoding: "utf8" })
+            .then((data) => JSON.parse(data))
+
+        const newUser = users.filter(user => {
+            return user.id == id
+        })
+        res.status(200).send(newUser)
+    }
+    catch (error) {
+        next({ status: 500, internalMessage: error.message });
+    }
+})
+
+// Get with age filter and return all users if no age is sent ====== BONUS ======
+router.get('/', async (req, res, next) => {
+    try {
+        const age = Number(req.query.age)
+        const users = await fs.promises
+            .readFile("./user.json", { encoding: "utf8" })
+            .then((data) => JSON.parse(data));
+        if (!age) {
+            return res.send(users)
+        }
+        const filteredUsers = users.filter(user => user.age === age)
+        res.send(filteredUsers)
+    } catch (error) {
+        next({ status: 500, internalMessage: error.message });
+    }
+
+});
 
 
+
+// Post register
+router.post("/", validateUser, async (req, res, next) => {
+    try {
+        const { username, age, password } = req.body;
+        const data = await fs.promises
+            .readFile("./user.json", { encoding: "utf8" })
+            .then((data) => JSON.parse(data));
+        const id = uuidv4();
+        data.push({ id, username, age, password });
+        await fs.promises.writeFile("./user.json", JSON.stringify(data), {
+            encoding: "utf8",
+        });
+        res.send({ id, message: "sucess" });
+    } catch (error) {
+        next({ status: 500, internalMessage: error.message });
+    }
+});
+
+
+// Post login
+router.post("/login", validateUser, (req, res) => {
+    res.status(401).send({ error: "failed" })
+})
+
+
+// Patch edit user by id
+router.patch("/:id", validateUser, async (req, res, next) => {
+    try {
+        const id = req.params.id
+        var { username, password, age } = req.body
+        const users = await fs.promises.readFile('./user.json', { encoding: "utf8" })
+            .then((data) => JSON.parse(data))
+        const newUsers = users.map(user => {
+            if (user.id !== id) return user;
+            return {
+                username, password, age, id
+            }
+        })
+        await fs.promises.writeFile("./user.json", JSON.stringify(newUsers), (err) => {
+            if (!err) return res.status(200).send({ message: "success" })
+            res.status(500).send("server error")
+        })
+        res.status(200).send({ message: "user edited" })
+    }
+    catch (error) {
+        next({ status: 500, internalMessage: error.message });
+    }
+});
+
+
+// Delete user by id
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const id = req.params.id
+        console.log(id);
+        const users = await fs.promises.readFile('./user.json', { encoding: "utf8" })
+            .then((data) => JSON.parse(data))
+
+        const newUsers = users.filter(user => {
+            return user.id !== id
+        })
+        await fs.promises.writeFile("./user.json", JSON.stringify(newUsers), (err) => {
+            if (!err) return res.status(200).send({ message: "success" })
+            res.status(500).send("server error")
+        })
+        res.status(200).send({ message: "user deleted" })
+
+    }
+    catch (error) {
+        next({ status: 500, internalMessage: error.message });
+    }
+})
 
 
 
--- userHelpers.js
+++ userHelpers.js
@@ -7,7 +7,9 @@ const validateUser = async (req, res, next) =>{
         const data = await fs.promises.readFile('./user.json',{encoding:'utf8'})
         const users = JSON.parse(data)
         const isUsernameExists = users.some(user=>user.username===username)
-        if(isUsernameExists && req.method == "POST") return next({status:422, message:"username is used"})
+        const isUserExists = users.some(user=>user.username===username && user.password===password)
+        if(isUsernameExists && req.method == "POST" && req.url == '/') return next({status:422, message:"username is used"})
+        if(isUserExists && req.url == '/login')return res.status(200).send({ message: "login success" });
         next()
     } catch (error) {
         next({status:500, internalMessage:error.message})
