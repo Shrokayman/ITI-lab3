a88013e977265644cfa7dcc71997d6a8717dc1f4
Shrok Ayman <shrokayman99@gmail.com>
2022-01-19 16:27:18 +0200

    Lab3 and Lab4 added BONUS

--- index.js
+++ index.js
@@ -15,6 +15,22 @@ app.use(bodyParser.json())
 https://www.youtube.com/playlist?list=PLdRrBA8IaU3Xp_qy8X-1u-iqeLlDCmR8a
 
 
+LAB3 & LAB4
+
+Create server with the following end points 
+POST /users with uuid, unique username 
+PATCH /users/id 
+GET /users with age filter 
+Create Error handler 
+POST /users/login /sucess 200 , error:403
+GET /users/id   200,   eror:404
+DELETE users/id  200,    error:404
+complete middleware for validating user
+Create Route For users 
+
+Bonus
+Edit patch end point to handle the sent data only
+If age is not sent return all users
 
 
 git add .
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -41,7 +41,28 @@ router.get('/', async (req, res, next) => {
 
 });
 
-
+// Patch edit user by id and modify the only data sent ======= Bonus ========
+router.patch("/:id", validateUser, async (req, res, next) => {
+    try {
+        const id = req.params.id
+        var { username, password, age } = req.body
+        const users = await fs.promises.readFile('./user.json', { encoding: "utf8" })
+            .then((data) => JSON.parse(data))
+        const newUser = users.find(user => user.id == id)
+        if (username) newUser.username = username
+        if (password) newUser.password = password
+        if (age) newUser.age = age
+        
+        await fs.promises.writeFile("./user.json", JSON.stringify(users), (err) => {
+            if (!err) return res.status(200).send({ message: "success" })
+            res.status(500).send("server error")
+        })
+        res.status(200).send({ message: "user edited" })
+    }
+    catch (error) {
+        next({ status: 500, internalMessage: error.message });
+    }
+});
 
 // Post register
 router.post("/", validateUser, async (req, res, next) => {
@@ -67,30 +88,6 @@ router.post("/login", validateUser, (req, res) => {
     res.status(401).send({ error: "failed" })
 })
 
-
-// Patch edit user by id
-router.patch("/:id", validateUser, async (req, res, next) => {
-    try {
-        const id = req.params.id
-        var { username, password, age } = req.body
-        const users = await fs.promises.readFile('./user.json', { encoding: "utf8" })
-            .then((data) => JSON.parse(data))
-        const newUsers = users.map(user => {
-            if (user.id !== id) return user;
-            return {
-                username, password, age, id
-            }
-        })
-        await fs.promises.writeFile("./user.json", JSON.stringify(newUsers), (err) => {
-            if (!err) return res.status(200).send({ message: "success" })
-            res.status(500).send("server error")
-        })
-        res.status(200).send({ message: "user edited" })
-    }
-    catch (error) {
-        next({ status: 500, internalMessage: error.message });
-    }
-});
 
 
 // Delete user by id
